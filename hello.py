69cf5806c4004973f2002070064c7c89a700f55b
StepanenkoKirill <123152402+StepanenkoKirill@users.noreply.github.com>
2023-09-10 23:24:10 +0300

    gcd

--- hello.py
+++ hello.py
@@ -5,3 +5,10 @@ def fun1():
     while(i < 5):
         i+=1
     print(i)
+def fun2(a,b):
+    while(a != 0 and b != 0):
+        if(a > b):
+            a = a % b
+        else:
+            b = b % a
+    return a + b
